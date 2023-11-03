334e9043bfe34c51c2b9e9a187f4bd3b830b6fc1
RayyanMasood <syed.rayyan.masood@gmail.com>
2023-11-04 02:25:38 +0500

    Committing changes and adding joke_script Committing changes and adding joke_script

---
+++ joke_script.py
@@ -0,0 +1,13 @@
+import random
+
+def tell_a_joke():
+    jokes = [
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
+        "Why don't some fish play piano? Because you can't tuna fish!",
+        "Why did the bicycle fall over? Because it was two-tired!"
+    ]
+    return random.choice(jokes)
+
+if __name__ == "__main__":
+    print(tell_a_joke())
